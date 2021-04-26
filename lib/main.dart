828a2c8c1706f4143605c532434f247f51f9e98c
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2021-04-26 21:45:57 +0430

    some texts added to buttons

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignupScreen(),
+      home:SignUpScreen() ,
     );
   }
 }
