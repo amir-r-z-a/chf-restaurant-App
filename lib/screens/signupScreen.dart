5173b3cfc373b2ae45c7c19eabc24967de6d4541
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2021-04-26 20:01:40 +0430

    textFormFields added to signUpScreen.dart

--- lib/screens/signupScreen.dart
+++ lib/screens/signupScreen.dart
@@ -17,24 +17,53 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           children: [
             Container(
-                 margin: EdgeInsets.fromLTRB(25, 30, 0, 0),
+                margin: EdgeInsets.fromLTRB(25, 50, 0, 0),
                 child: Row(
                   children: [
                     Text(
                       "Sign up",
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                     ),
                   ],
                 )),
             Container(
               margin: EdgeInsets.all(30),
               child: Form(
-                  key: _formkey,
-                  child: Column(
-                    children: [
-                      MyTextFormField("salam" , hint: "hi",)
-                    ],
-                  ),
+                key: _formkey,
+                child: Column(
+                  children: [
+                    MyTextFormField(
+                      "Name",
+                      hint: "your name",
+                    ),
+                    Padding(padding: EdgeInsets.all(15)),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(248, 95, 106, 1)),
+                        hintText: "your password ",
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          color: Color.fromRGBO(209, 214, 219, 1),
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(15)),
+                    MyTextFormField(
+                      "Phone number",
+                      hint: "your Phone number",
+                    ),
+                    Padding(padding: EdgeInsets.all(15)),
+                    MyTextFormField(
+                      "Address",
+                      hint: "your Address",
+                    )
+                  ],
+                ),
               ),
             )
           ],
