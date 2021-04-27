d8333724530ea4db441d3803d181802d53a7d66f
arman heidari <armanartgallery@gmail.com>
2021-04-27 14:06:51 +0430

    MyTextFormField updated

--- lib/Common/Text/MyTextFormField.dart
+++ lib/Common/Text/MyTextFormField.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
 
 class MyTextFormField extends StatelessWidget {
-  String str;
+  static String name;
+  static String password;
+  static String phoneNumber;
+  static String address;
   String label;
   String hint;
+  String valid;
+  int index;
 
-  MyTextFormField(this.label, {this.hint});
+  MyTextFormField(this.index, this.label, {this.hint, this.valid});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      // style: ,
       cursorColor: Color.fromRGBO(248, 95, 106, 1),
       onSaved: (String value) {
-        this.str = value;
+        if (index == 1) {
+          name = value;
+        } else if (index == 2) {
+          password = value;
+        } else if (index == 3) {
+          phoneNumber = value;
+        } else {
+          address = value;
+        }
+      },
+      validator: (String value) {
+        if (valid == null && (value == null || value.isEmpty)) {
+          return "Please enter something";
+        } else if (false /*value != valid*/) {
+          return "Your enter is incorrect";
+        }
+        return null;
       },
       decoration: InputDecoration(
+        errorStyle: TextStyle(color: Color.fromRGBO(248, 95, 106, 1)),
         labelText: label,
         labelStyle: TextStyle(
             fontSize: 14,
