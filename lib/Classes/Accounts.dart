249cbe22d5e0f1e4d968dc8d4fe53184d55b064e
arman heidari <armanartgallery@gmail.com>
2021-04-27 19:33:24 +0430

    Account and Restaurant updated

--- lib/Classes/Accounts.dart
+++ lib/Classes/Accounts.dart
@@ -2,6 +2,7 @@ import 'package:chfrestaurant/Classes/Restaurant.dart';
 
 class Accounts {
   static List<Restaurant> _accounts = List.empty(growable: true);
+  static int _currentAccount;
 
   static void addAccount(Restaurant restaurant) {
     _accounts.add(restaurant);
@@ -11,9 +12,34 @@ class Accounts {
     return _accounts.length;
   }
 
+  static bool foundPhoneNumber(String input) {
+    for (int i = 0; i < getLength(); i++) {
+      if (accounts[i].phoneNumber == input) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  static bool foundPassword(String input) {
+    for (int i = 0; i < getLength(); i++) {
+      if (accounts[i].password == input) {
+        currentAccount = i;
+        return false;
+      }
+    }
+    return true;
+  }
+
   static List<Restaurant> get accounts => _accounts;
 
   static set accounts(List<Restaurant> value) {
     _accounts = value;
+  }
+
+  static int get currentAccount => _currentAccount;
+
+  static set currentAccount(int value) {
+    _currentAccount = value;
   }
 }
--- lib/Classes/Restaurant.dart
+++ lib/Classes/Restaurant.dart
@@ -3,23 +3,26 @@
 import 'package:chfrestaurant/Classes/ClientComment.dart';
 import 'package:chfrestaurant/Classes/Food.dart';
 import 'package:chfrestaurant/Classes/Order.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 
 class Restaurant {
-  String _name = 'a';
-  String _phoneNumber = 'a';
-  String _password = 'a';
-  String _address = 'a';
-  String _type = 'a';
+  String _name;
+  String _phoneNumber;
+  String _password;
+  String _address;
+  String _type;
+  List<Food> _foods;
+  double _workingRadius = 10;
+  double _point;
+  List<Order> _orderHistory;
+  List<ClientComment> _restaurantComments;
 
-  // List<Food> _foods;
-  // double _workingRadius;
+  Restaurant(
+      this._name, this._phoneNumber, this._password, this._address, this._type);
+
   // Image _profileImage;
 
   // Location _location;
-  // double _point;
-  // List<Order> _orderHistory;
-  // List<ClientComment> _restaurantComments;
 
   String get name => _name;
 
@@ -51,45 +54,45 @@ class Restaurant {
     _type = value;
   }
 
-  // List<Food> get foods => _foods;
-  //
-  // set foods(List<Food> value) {
-  //   _foods = value;
-  // }
-  //
-  // double get workingRadius => _workingRadius;
-  //
-  // set workingRadius(double value) {
-  //   _workingRadius = value;
-  // }
-  //
-  // Image get profileImage => _profileImage;
-  //
-  // set profileImage(Image value) {
-  //   _profileImage = value;
-  // }
+  List<Food> get foods => _foods;
 
-  // Location get location => _location;
-  //
-  // set location(Location value) {
-  //   _location = value;
-  // }
+  set foods(List<Food> value) {
+    _foods = value;
+  }
 
-  // double get point => _point;
-  //
-  // set point(double value) {
-  //   _point = value;
-  // }
-  //
-  // List<Order> get orderHistory => _orderHistory;
-  //
-  // set orderHistory(List<Order> value) {
-  //   _orderHistory = value;
-  // }
-  //
-  // List<ClientComment> get restaurantComments => _restaurantComments;
-  //
-  // set restaurantComments(List<ClientComment> value) {
-  //   _restaurantComments = value;
-  // }
+  double get workingRadius => _workingRadius;
+
+  set workingRadius(double value) {
+    _workingRadius = value;
+  }
+
+// Image get profileImage => _profileImage;
+//
+// set profileImage(Image value) {
+//   _profileImage = value;
+// }
+//
+// Location get location => _location;
+//
+// set location(Location value) {
+//   _location = value;
+// }
+
+  double get point => _point;
+
+  set point(double value) {
+    _point = value;
+  }
+
+  List<Order> get orderHistory => _orderHistory;
+
+  set orderHistory(List<Order> value) {
+    _orderHistory = value;
+  }
+
+  List<ClientComment> get restaurantComments => _restaurantComments;
+
+  set restaurantComments(List<ClientComment> value) {
+    _restaurantComments = value;
+  }
 }
