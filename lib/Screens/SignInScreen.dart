e3ea912d02b9ae3e6b4f0d98da045734ee524f54
arman heidari <armanartgallery@gmail.com>
2021-04-27 14:09:06 +0430

    MyTextFormField updated

--- lib/Screens/SignInScreen.dart
+++ lib/Screens/SignInScreen.dart
@@ -7,18 +7,35 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  var key1 = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        MyTextFormField(
-          "Name",
-          hint: "hiiiii",
-        ),
-        MyTextFormField(
-          "Password",
-        ),
-      ]),
+      body: Form(
+        key: key1,
+        child: ListView(children: [
+          MyTextFormField(
+            1,
+            "Name",
+            hint: "hiiiii",
+          ),
+          MyTextFormField(
+            2,
+            "password",
+          ),
+          ElevatedButton(
+              onPressed: () {
+                if (key1.currentState.validate()) {
+                  key1.currentState.save();
+                  print("done");
+                  print(MyTextFormField.name);
+                  print(MyTextFormField.password);
+                }
+              },
+              child: Text("Continue"))
+        ]),
+      ),
     );
   }
 }
