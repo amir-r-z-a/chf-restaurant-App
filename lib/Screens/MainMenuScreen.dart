23412c4db1171c9c93b3592d87b84fbb7b5ae6bb
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2021-05-03 22:30:53 +0430

    mainScreen class navigation bar added and bottoms text added

--- lib/Screens/MainMenuScreen.dart
+++ lib/Screens/MainMenuScreen.dart
@@ -1,86 +1,175 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 class MainMenuScreen extends StatefulWidget {
   @override
   _MainMenuScreenState createState() => _MainMenuScreenState();
 }
-
 class _MainMenuScreenState extends State<MainMenuScreen> {
+  int _currentSelected = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Container(
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentSelected,
+          onTap: (value) {
+            setState(() => _currentSelected = value);
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_restaurant_sharp),
+              label: 'Orders',
+              backgroundColor: Colors.green,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_outlined),
+              label: 'DashBoard',
+              backgroundColor: Colors.purple,
+            ),
+          ],
+        ),
         drawer: Container(
           child: Text("here is a drawer"),
         ),
-       appBar: AppBar(
-         title: Text("hi"),
-       ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Main Menu"),
+        ),
         body: Container(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(30, 60, 0,0),
-                child: Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: Colors.pink),
-                        borderRadius: const BorderRadius.all(const Radius.circular(8))
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Menu Edition"),
+                            ),
+                            Container(
+                              child: Icon(
+                                Icons.edit_rounded,
+                                size: 35,
+                              ),
+                              margin: EdgeInsets.only(left: 20),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.pink),
+                            borderRadius:
+                                const BorderRadius.all(const Radius.circular(8))),
+                        width: 170,
+                        height: 120,
                       ),
-                      width: 150,
-                      height: 90,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 60,30,0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: Colors.pink),
-                          borderRadius: const BorderRadius.all(const Radius.circular(8))
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 30, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Orders History"),
+                            ),
+                            Container(
+                              child: Icon(
+                                Icons.edit_rounded,
+                                size: 35,
+                              ),
+                              margin: EdgeInsets.only(left: 20),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.pink),
+                            borderRadius:
+                                const BorderRadius.all(const Radius.circular(8))),
+                        width: 170,
+                        height: 120,
                       ),
-                      width: 150,
-                      height: 90,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(30, 60, 0,0),
-                child: Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: Colors.pink),
-                          borderRadius: const BorderRadius.all(const Radius.circular(8))
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Hesabdari"),
+                            ),
+                            Container(
+                              child: Icon(
+                                Icons.edit_rounded,
+                                size: 35,
+                              ),
+                              margin: EdgeInsets.only(left: 20),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.pink),
+                            borderRadius:
+                                const BorderRadius.all(const Radius.circular(8))),
+                        width: 170,
+                        height: 120,
                       ),
-                      width: 150,
-                      height: 90,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 60, 30,0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: Colors.pink),
-                          borderRadius: const BorderRadius.all(const Radius.circular(8))
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 30, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Comments"),
+                            ),
+                            Container(
+                              child: Icon(
+                                Icons.edit_rounded,
+                                size: 35,
+                              ),
+                              margin: EdgeInsets.only(left: 20),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.pink),
+                            borderRadius:
+                                const BorderRadius.all(const Radius.circular(8))),
+                        width: 170,
+                        height: 120,
                       ),
-                      width: 150,
-                      height: 90,
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
