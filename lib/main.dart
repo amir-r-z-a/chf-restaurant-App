221bd7fbfc524884991abfbcc077ad8e65acf95c
arman heidari <armanartgallery@gmail.com>
2021-05-05 22:42:39 +0430

    Main updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,31 @@ import 'package:chfrestaurant/Classes/Accounts.dart';
 import 'package:chfrestaurant/Classes/Food.dart';
 import 'package:chfrestaurant/Classes/Restaurant.dart';
 import 'package:chfrestaurant/Classes/RestaurantTypes.dart';
+import 'package:chfrestaurant/Screens/DetailsFoodTile.dart';
 import 'package:chfrestaurant/Screens/MainMenuScreen.dart';
-import 'package:chfrestaurant/Screens/MenuOrder.dart';
+import 'package:chfrestaurant/Screens/OrdersMenu.dart';
 import 'package:chfrestaurant/Screens/SignInScreen.dart';
 import 'package:chfrestaurant/Screens/SignUpScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Restaurant restaurant = Restaurant(
+      'name1', '09198612878', 'password1', 'addres1', RestaurantTypes.Cafe);
+  Accounts.addAccount(restaurant);
+  restaurant.addTabBarTitle(
+      'Cafe',
+      FoodTile(
+        'Tea',
+        25,
+        true,
+        desc: 'desc',
+      ));
+  print(Accounts.currentAccount);
+  print(Accounts.accounts.length);
+  print(restaurant.tabBarTitle);
+  print(restaurant.tabBarView);
   // Accounts.addAccount(Restaurant(
-  //     'name1', 'phoneNumber1', 'password1', 'addres1', RestaurantTypes.Cafe));
-  // Accounts.addAccount(Restaurant(
-  //     'name2', 'phoneNumber2', 'password2', 'addres2', RestaurantTypes.Other));
+  //     'name2', 09121111111, 'password2', 'addres2', RestaurantTypes.Other));
   runApp(MyApp());
 }
 
@@ -25,30 +39,25 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "ChFood",
-      theme: ThemeData(primaryColor: const Color.fromRGBO(248, 95, 106, 1)),
-      routes: {
-        '/SignInScreen': (context) => SignInScreen(),
-        '/SignUpScreen': (context) => SignUpScreen()
-      },
-      home:
-          /*Scaffold(
-          body: Chip(
-        avatar: CircleAvatar(
-          backgroundColor: Colors.green,
-          foregroundColor: Colors.red,
-          radius: 1,
-          child: Text('c'),
-        ),
-        label: Text('Cafe'),
-      ))*/
-          FoodTile(
+        title: "ChFood",
+        theme: ThemeData(primaryColor: const Color.fromRGBO(248, 95, 106, 1)),
+        routes: {
+          '/SignInScreen': (context) => SignInScreen(),
+          '/SignUpScreen': (context) => SignUpScreen(),
+          '/DetailsFoodTile': (context) => DetailsFoodTile(),
+        },
+        home:
+            OrdersMenu() /*SignInScreen()*/ /*FoodTile(
+          'Egg',
+          28,
+          true,
+          desc: 'xx',
+        )*/
+        /*DetailsFoodTile(
         'Egg',
         28,
-        true,
-        desc: 'desc'
-          /*MainMenuScreen()*/
-          /*SignInScreen()*/ /*MenuOrder()*/,
-    ));
+        desc: 'xx',
+      ),*/
+        );
   }
 }
