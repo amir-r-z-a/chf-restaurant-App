e5b131a46fc80bfe6d8f79ceb3bc1491637d11ce
arman heidari <armanartgallery@gmail.com>
2021-05-05 22:41:31 +0430

    Restaurant updated

--- lib/Classes/Restaurant.dart
+++ lib/Classes/Restaurant.dart
@@ -1,10 +1,7 @@
-// import 'dart:html';
-
 import 'package:chfrestaurant/Classes/ClientComment.dart';
 import 'package:chfrestaurant/Classes/Food.dart';
 import 'package:chfrestaurant/Classes/Order.dart';
 import 'package:chfrestaurant/Classes/RestaurantTypes.dart';
-// import 'package:flutter/cupertino.dart';
 
 class Restaurant {
   String _name;
@@ -12,9 +9,11 @@ class Restaurant {
   String _password;
   String _address;
   RestaurantTypes _type;
-  List<Food> _foods;
+  Map _tabBarView = Map();
+  Map _tabBarTitle = Map();
   double _workingRadius = 10;
   double _point;
+  String email;
   List<Order> _orderHistory;
   List<ClientComment> _restaurantComments;
 
@@ -24,6 +23,45 @@ class Restaurant {
   // Image _profileImage;
 
   // Location _location;
+
+  int getTabBarTitleLength() {
+    return tabBarTitle.length;
+  }
+
+  int getAllFoodsLength() {
+    int counter = 0;
+    for (int i = 0; i < getTabBarTitleLength(); i++) {
+      counter += tabBarView[i].length;
+    }
+    return counter;
+  }
+
+  bool validFood(String input) {
+    for (int i = 0; i < getTabBarTitleLength(); i++) {
+      for (int j = 0; j < tabBarView[i].length; j++) {
+        if (tabBarView[i][j].name == input) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void addTabBarViewElements(FoodTile food, int i) {
+    tabBarView[i].add(food);
+  }
+
+  void addTabBarTitle(String title, FoodTile food) {
+    int len = getTabBarTitleLength();
+    tabBarTitle[len] = title;
+    tabBarView[len] = [];
+    addTabBarViewElements(food, len);
+  }
+
+  //order history majmooe sefareshat active va qeir active ast behtar ast do fiel shavad
+  int getOrderHistoryLength() {
+    return orderHistory.length;
+  }
 
   String get name => _name;
 
@@ -49,17 +87,16 @@ class Restaurant {
     _address = value;
   }
 
-
   RestaurantTypes get type => _type;
 
   set type(RestaurantTypes value) {
     _type = value;
   }
 
-  List<Food> get foods => _foods;
+  Map get tabBarView => _tabBarView;
 
-  set foods(List<Food> value) {
-    _foods = value;
+  set tabBarView(Map value) {
+    _tabBarView = value;
   }
 
   double get workingRadius => _workingRadius;
@@ -90,6 +127,12 @@ class Restaurant {
 
   set orderHistory(List<Order> value) {
     _orderHistory = value;
+  }
+
+  Map get tabBarTitle => _tabBarTitle;
+
+  set tabBarTitle(Map value) {
+    _tabBarTitle = value;
   }
 
   List<ClientComment> get restaurantComments => _restaurantComments;
