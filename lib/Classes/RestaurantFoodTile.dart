12549c84e89f56527ad87e2c6ac374925d551406
arman heidari <armanartgallery@gmail.com>
2021-05-07 20:18:39 +0430

    RestaurantFoodTile added

---
+++ lib/Classes/RestaurantFoodTile.dart
@@ -0,0 +1,237 @@
+import 'package:chfrestaurant/Classes/Accounts.dart';
+import 'package:chfrestaurant/Screens/DetailsRestaurantFoodTile.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantFoodTile extends StatefulWidget {
+  String _name;
+  String _foodType;
+  String _price;
+  String desc;
+  bool _foodStatus;
+  Image _foodImage;
+  static Function function;
+
+  String get name => _name;
+
+  set name(String value) {
+    _name = value;
+  }
+
+  RestaurantFoodTile(this._name, this._price, this._foodStatus, {this.desc});
+
+  @override
+  _RestaurantFoodTileState createState() => _RestaurantFoodTileState();
+
+  String get price => _price;
+
+  set price(String value) {
+    _price = value;
+  }
+
+  bool get foodStatus => _foodStatus;
+
+  set foodStatus(bool value) {
+    _foodStatus = value;
+  }
+
+  String get foodType => _foodType;
+
+  set foodType(String value) {
+    _foodType = value;
+  }
+
+  Image get foodImage => _foodImage;
+
+  set foodImage(Image value) {
+    _foodImage = value;
+  }
+}
+
+class _RestaurantFoodTileState extends State<RestaurantFoodTile> {
+  void refreshPage() {
+    setState(() {
+      widget.name = DetailsRestaurantFoodTile.name;
+      widget.desc = DetailsRestaurantFoodTile.desc;
+      widget.price = DetailsRestaurantFoodTile.price;
+      widget.foodStatus = DetailsRestaurantFoodTile.foodStatus;
+    });
+  }
+
+  void deleteFunc(bool flag) {
+    showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Delete Food'),
+            content: Container(
+              height: 100,
+              child: Column(children: [
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(1)),
+                    Text('Are you sure to delete ' +
+                        widget.name +
+                        ' in ' +
+                        Accounts.accounts[Accounts.currentAccount]
+                            .findCategory(widget.name) +
+                        '?'),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(0, 30, 0, 0)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        print('--------------------------------------');
+                        print(Accounts.accounts[Accounts.currentAccount]
+                            .restaurantTabBarView);
+
+                        Accounts.accounts[Accounts.currentAccount]
+                            .deleteTabBarViewElements(widget.name);
+
+                        print(Accounts.accounts[Accounts.currentAccount]
+                            .restaurantTabBarView);
+                        print('--------------------------------------');
+
+                        RestaurantFoodTile.function();
+                        Navigator.pop(context);
+                        if (flag) {
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text(
+                        'Delete',
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                  ],
+                )
+              ]),
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        DetailsRestaurantFoodTile.deleteFunction = deleteFunc;
+        DetailsRestaurantFoodTile.name = widget.name;
+        DetailsRestaurantFoodTile.desc =
+            widget.desc != null ? widget.desc : ' ';
+        DetailsRestaurantFoodTile.price = widget.price;
+        DetailsRestaurantFoodTile.foodStatus = widget.foodStatus;
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailsRestaurantFoodTile(
+              function: refreshPage,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.symmetric(
+              horizontal: BorderSide(color: Color(0xffDCDCDC), width: 1)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 3)),
+            Row(children: [
+              Padding(padding: EdgeInsets.all(7)),
+              Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                height: 95,
+                width: 95,
+              ),
+              Padding(padding: EdgeInsets.all(5)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(widget.name),
+                  Padding(padding: EdgeInsets.all(3)),
+                  Text(widget.desc == null ? ' ' : widget.desc),
+                  Padding(padding: EdgeInsets.all(13)),
+                  Text('Price: ' + widget.price.toString() + '\$'),
+                ],
+              ),
+            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(3)),
+                    SizedBox(
+                      // height: 10,
+                      // width: 100,
+                      child: Switch(
+                        inactiveThumbColor: Colors.red,
+                        inactiveTrackColor: Colors.red,
+                        activeTrackColor: Color.fromRGBO(0, 181, 0, 1),
+                        activeColor: Color.fromRGBO(0, 181, 0, 1),
+                        value: widget.foodStatus,
+                        onChanged: (value) {
+                          setState(() {
+                            widget.foodStatus = !widget.foodStatus;
+                          });
+                        },
+                      ),
+                    ),
+                    Text(
+                      widget.foodStatus ? 'Active' : 'Inactive',
+                      style: TextStyle(
+                          color: widget.foodStatus ? Colors.green : Colors.red,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () => deleteFunc(false),
+                      child: Container(
+                        height: 25,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7),
+                          border: Border.all(width: 0.7, color: Colors.black),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Delete',
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(7)),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+        // color: Colors.green,
+        height: 190,
+        width: MediaQuery.of(context).size.width,
+      ),
+    );
+  }
+}
