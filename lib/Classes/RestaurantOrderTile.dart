20cbfeecfed64c25e33a42fe65aa5496b777a6dd
arman heidari <armanartgallery@gmail.com>
2021-05-08 01:19:49 +0430

    RestaurantOrderTile and DetailsRestaurantOrderTile added

---
+++ lib/Classes/RestaurantOrderTile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RestaurantOrderTile extends StatefulWidget {
+  @override
+  _RestaurantOrderTileState createState() => _RestaurantOrderTileState();
+}
+
+class _RestaurantOrderTileState extends State<RestaurantOrderTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Screens/DetailsRestaurantOrderTile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsRestaurantOrderTile extends StatefulWidget {
+  @override
+  _DetailsRestaurantOrderTileState createState() =>
+      _DetailsRestaurantOrderTileState();
+}
+
+class _DetailsRestaurantOrderTileState
+    extends State<DetailsRestaurantOrderTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
