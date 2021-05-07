978e446e63938b607fabfb9e5d1b6f977e6fe301
arman heidari <armanartgallery@gmail.com>
2021-05-08 01:20:57 +0430

    ClientOrderTile.dart added

---
+++ lib/Classes/ClientOrderTile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ClientOrderTile extends StatefulWidget {
+  @override
+  _ClientOrderTileState createState() => _ClientOrderTileState();
+}
+
+class _ClientOrderTileState extends State<ClientOrderTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
