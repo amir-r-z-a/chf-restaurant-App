8bb45329aa4cd03e9dc99ee1ecc92998897fcd14
arman heidari <armanartgallery@gmail.com>
2021-05-08 00:43:43 +0430

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:chfrestaurant/Classes/Accounts.dart';
-import 'package:chfrestaurant/Classes/Food.dart';
+import 'package:chfrestaurant/Classes/Comment.dart';
+import 'package:chfrestaurant/Classes/Date.dart';
 import 'package:chfrestaurant/Classes/Restaurant.dart';
+import 'package:chfrestaurant/Classes/RestaurantFoodTile.dart';
 import 'package:chfrestaurant/Classes/RestaurantTypes.dart';
-import 'package:chfrestaurant/Screens/DetailsFoodTile.dart';
+import 'package:chfrestaurant/Screens/CommentsManagement.dart';
+import 'package:chfrestaurant/Screens/DetailsRestaurantFoodTile.dart';
 import 'package:chfrestaurant/Screens/MainMenuScreen.dart';
 import 'package:chfrestaurant/Screens/OrdersMenu.dart';
 import 'package:chfrestaurant/Screens/ProfileScreen.dart';
@@ -16,16 +19,50 @@ void main() {
   Accounts.addAccount(restaurant);
   restaurant.addTabBarTitle(
       'Cafe',
-      FoodTile(
+      RestaurantFoodTile(
         'Tea',
         '25',
         true,
         desc: 'desc',
       ));
+  restaurant.addTabBarTitle(
+      'Fast Food',
+      RestaurantFoodTile(
+        'Hot Dog',
+        '25',
+        true,
+        desc: 'desc',
+      ));
+  restaurant.addComments(
+      CommentTile(
+          'chera qazatoon inqad bade?',
+          'Milk',
+          Date('1970', '12', '3', '22', '12', '2'),
+          '09121231212',
+          '09198612878'),
+      1);
+  restaurant.addComments(
+      CommentTile(
+          'chera qazatoon inqad khoobe?',
+          'Tea',
+          Date('1975', '1', '30', '2', '2', '12'),
+          '09121231212',
+          '09198612878'),
+      1);
+  restaurant.addComments(
+      CommentTile(
+          'chera qazatoon inqad bade va khoobe?',
+          'Hot Dog',
+          Date('2220', '12', '13', '14', '12', '29'),
+          '09121231212',
+          '09198612878'),
+      2);
   print(Accounts.currentAccount);
   print(Accounts.accounts.length);
   print(restaurant.tabBarTitle);
-  print(restaurant.tabBarView);
+  print(restaurant.restaurantTabBarView);
+  print(restaurant.listOfFood);
+  print(restaurant.restaurantComments);
   // Accounts.addAccount(Restaurant(
   //     'name2', 09121111111, 'password2', 'addres2', RestaurantTypes.Other));
   runApp(MyApp());
@@ -45,19 +82,19 @@ class _MyAppState extends State<MyApp> {
         routes: {
           '/SignInScreen': (context) => SignInScreen(),
           '/SignUpScreen': (context) => SignUpScreen(),
-          '/DetailsFoodTile': (context) => DetailsFoodTile(),
+          '/DetailsFoodTile': (context) => DetailsRestaurantFoodTile(),
           '/MainMenuScreen': (context) => MainMenuScreen(),
           '/ProfileScreen': (context) => ProfileScreen(),
           '/OrdersMenu': (context) => OrdersMenu(),
         },
         home:
-            SignInScreen() /*SignInScreen()*/ /*FoodTile(
+            /*CommentsManagement() */ /*OrdersMenu()*/ SignInScreen() /*FoodTile(
           'Egg',
           28,
           true,
           desc: 'xx',
         )*/
-        /*DetailsFoodTile(
+        /*DetailsRestaurantFoodTile(
         'Egg',
         28,
         desc: 'xx',
