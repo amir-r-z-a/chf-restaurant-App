2090d1835dc56000133be1e654173a7de5e831dd
arman heidari <armanartgallery@gmail.com>
2021-05-09 22:02:01 +0430

    validDate added

--- lib/Classes/Date.dart
+++ lib/Classes/Date.dart
@@ -12,15 +12,95 @@ class Date {
   String dateFormatter() {
     return year +
         '/' +
-        day +
-        '/' +
         month +
+        '/' +
+        day +
         ' , ' +
         hour +
         ':' +
         minute +
         ':' +
         second;
+  }
+
+  // bool validDate(Date input, int i) {
+  //   if (int.parse(input.year) != int.parse(year)) {
+  //     if (int.parse(input.year) < int.parse(year)) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   } else if (int.parse(input.month) != int.parse(month)) {
+  //     if (int.parse(input.month) < int.parse(month)) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   } else if (int.parse(input.day) != int.parse(day)) {
+  //     if (int.parse(input.day) < int.parse(day) + i) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   } else if (int.parse(input.hour) != int.parse(hour)) {
+  //     if (int.parse(input.hour) < int.parse(hour)) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   } else if (int.parse(input.minute) != int.parse(minute)) {
+  //     if (int.parse(input.minute) < int.parse(minute)) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   } else if (int.parse(input.second) != int.parse(second)) {
+  //     if (int.parse(input.second) < int.parse(second)) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+
+  bool validDate(Date input, int i) {
+    int x = int.parse(day);
+    for (int j = 1; j <= int.parse(input.month); j++) {
+      if (j <= 6 && j >= 1) {
+        input.day = (int.parse(input.day) + 31).toString();
+        x += 31;
+      } else if (j <= 11 && j >= 7) {
+        input.day = (int.parse(input.day) + 30).toString();
+        x += 30;
+      } else {
+        input.day = (int.parse(input.day) + 29).toString();
+        x += 29;
+      }
+    }
+    if (int.parse(input.year) == int.parse(year) &&
+        int.parse(input.day) - x <= i) {
+      if (int.parse(input.day) - x < i) {
+        return true;
+      } else if (int.parse(input.day) - x == i &&
+          int.parse(hour) <= int.parse(input.hour)) {
+        if (int.parse(hour) < int.parse(input.hour)) {
+          return true;
+        } else if (int.parse(hour) == int.parse(input.hour) &&
+            int.parse(minute) <= int.parse(input.minute)) {
+          if (int.parse(minute) < int.parse(input.minute)) {
+            return true;
+          } else if (int.parse(minute) == int.parse(input.minute) &&
+              int.parse(second) <= int.parse(input.second)) {
+            return true;
+          }
+          return false;
+        }
+        return false;
+      }
+      return false;
+    }
+    return false;
   }
 
   String get year => _year;
@@ -59,3 +139,30 @@ class Date {
     _month = value;
   }
 }
+
+//  bool validDate(Date input, int i) {
+//     if (int.parse(input.year) == int.parse(year) &&
+//         int.parse(input.month) == int.parse(month) &&
+//         int.parse(input.day) - int.parse(day) <= i) {
+//       if (int.parse(input.day) - int.parse(day) < i) {
+//         return true;
+//       } else if (int.parse(input.day) - int.parse(day) == i &&
+//           int.parse(hour) <= int.parse(input.hour)) {
+//         if (int.parse(hour) < int.parse(input.hour)) {
+//           return true;
+//         } else if (int.parse(hour) == int.parse(input.hour) &&
+//             int.parse(minute) <= int.parse(input.minute)) {
+//           if (int.parse(minute) < int.parse(input.minute)) {
+//             return true;
+//           } else if (int.parse(minute) == int.parse(input.minute) &&
+//               int.parse(second) <= int.parse(input.second)) {
+//             return true;
+//           }
+//           return false;
+//         }
+//         return false;
+//       }
+//       return false;
+//     }
+//     return false;
+//   }
