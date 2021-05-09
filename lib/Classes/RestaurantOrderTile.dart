7724c88330b806fcc9be16f97c1da0888feea2c3
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2021-05-09 16:23:37 +0430

    tile built for an order

--- lib/Classes/RestaurantOrderTile.dart
+++ lib/Classes/RestaurantOrderTile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantOrderTile extends StatefulWidget {
@@ -6,8 +7,71 @@ class RestaurantOrderTile extends StatefulWidget {
 }
 
 class _RestaurantOrderTileState extends State<RestaurantOrderTile> {
+  String SelectedValue ;
+  List panelItems =["active","unActive","finished"];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+          child: GestureDetector(
+            onTap: (){print("taped");},
+            child: SizedBox(
+              height:200,
+              child: Card(
+                child: ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Amirreza' + '  Ahmadi'),
+                        Text("09185452060"),
+                  ]
+                  ),
+                   subtitle:Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                       children:[
+                         Column(
+                           children: [
+                             Text("#9956"),
+                           ],
+                         ),
+                         Column(
+                           children: [
+                             Text("2020/05/05"),
+                             Container(
+                               margin: EdgeInsets.only(top: 70),
+                               padding: EdgeInsets.all(5),
+                               decoration: BoxDecoration(
+                                 border: Border.all(color: Colors.grey),
+                                 borderRadius: BorderRadius.circular(15)
+                               ),
+                               child: DropdownButton(
+
+                                 hint: Text("Order Status "),
+                                 dropdownColor: Colors.grey,
+                                 icon:Icon (Icons.arrow_drop_down),
+                                 value: SelectedValue,
+                                 onChanged: (newValue){
+                                   setState(() {
+                                     SelectedValue=newValue;
+                                   });
+                                 },
+                                 items: panelItems.map((valueitem){
+                                   return DropdownMenuItem(
+                                     value: valueitem,
+                                       child: Text(valueitem),
+                                   );
+                                 }
+                                 ).toList(),
+                               ),
+                             )
+                           ],
+                         )
+                   ] ),
+                ),
+              ),
+            ),
+          ),
+        ),
+    );
   }
 }
