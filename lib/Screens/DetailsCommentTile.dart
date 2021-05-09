1bec4c3740269f4f1d03bdced7b2d4ed0ebbabe1
arman heidari <armanartgallery@gmail.com>
2021-05-09 23:15:40 +0430

    DetailsCommentTile.dart added

---
+++ lib/Screens/DetailsCommentTile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailsCommentTile extends StatefulWidget {
+  @override
+  _DetailsCommentTileState createState() => _DetailsCommentTileState();
+}
+
+class _DetailsCommentTileState extends State<DetailsCommentTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
