7d9d496d0d859918bc7235ec53184835dd50c41f
arman heidari <armanartgallery@gmail.com>
2021-05-11 17:59:02 +0430

    Navigator added

--- lib/Screens/MainMenuScreen.dart
+++ lib/Screens/MainMenuScreen.dart
@@ -2,25 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class customListTile extends StatefulWidget {
-   IconData icon ;
-   String text ;
-  Function ontap ;
+  IconData icon;
 
-   customListTile(this.icon, this.text, this.ontap);
+  String text;
+
+  Function ontap;
+
+  customListTile(this.icon, this.text, this.ontap);
 
   @override
   _customListTileState createState() => _customListTileState();
 }
 
 class _customListTileState extends State<customListTile> {
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
       child: InkWell(
         splashColor: Colors.deepOrange,
-        onTap:widget.ontap,
+        onTap: widget.ontap,
         child: Container(
           height: 40,
           child: Row(
@@ -63,7 +64,14 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
           type: BottomNavigationBarType.fixed,
           currentIndex: _currentSelected,
           onTap: (value) {
-            setState(() => _currentSelected = value);
+            setState(() {
+              _currentSelected = value;
+              if (_currentSelected == 0) {
+                Navigator.pushNamed(context, '/MainMenuScreen');
+              } else if (_currentSelected == 1) {
+                Navigator.pushNamed(context, '/ActiveOrdersScreen');
+              }
+            });
           },
           items: [
             BottomNavigationBarItem(
@@ -93,9 +101,10 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                   Colors.deepOrange,
                 ])),
                 child: Text('here is header')),
-            customListTile(Icons.person,'Profile',()=>{Navigator.pushNamed(context, '/ProfileScreen')}),
-            customListTile(Icons.phone,'Contact Us',()=>{}),
-            customListTile(Icons.logout,"Log Out",()=>{}),
+            customListTile(Icons.person, 'Profile',
+                () => {Navigator.pushNamed(context, '/ProfileScreen')}),
+            customListTile(Icons.phone, 'Contact Us', () => {}),
+            customListTile(Icons.logout, "Log Out", () => {}),
           ],
         )),
         appBar: AppBar(
@@ -110,9 +119,10 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                   margin: EdgeInsets.fromLTRB(30, 20, 0, 0),
                   child: Row(
                     children: [
-                      GestureDetector(onTap: () {
-                        Navigator.pushNamed(context, '/OrdersMenu');
-                      },
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/OrdersMenu');
+                        },
                         child: Container(
                           child: Row(
                             children: [
@@ -145,28 +155,33 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(left: 20),
-                              child: Text("Orders History"),
-                            ),
-                            Container(
-                              child: Icon(
-                                Icons.edit_rounded,
-                                size: 35,
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/OrdersHistoryScreen');
+                        },
+                        child: Container(
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 20),
+                                child: Text("Orders History"),
                               ),
-                              margin: EdgeInsets.only(left: 20),
-                            ),
-                          ],
+                              Container(
+                                child: Icon(
+                                  Icons.edit_rounded,
+                                  size: 35,
+                                ),
+                                margin: EdgeInsets.only(left: 20),
+                              ),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 1, color: Colors.pink),
+                              borderRadius: const BorderRadius.all(
+                                  const Radius.circular(8))),
+                          width: 170,
+                          height: 120,
                         ),
-                        decoration: BoxDecoration(
-                            border: Border.all(width: 1, color: Colors.pink),
-                            borderRadius: const BorderRadius.all(
-                                const Radius.circular(8))),
-                        width: 170,
-                        height: 120,
                       ),
                     ],
                   ),
@@ -192,9 +207,10 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                           ],
                         ),
                         decoration: BoxDecoration(
-                            border: Border.all(width: 1, color: Colors.pink),
-                            borderRadius: const BorderRadius.all(
-                                const Radius.circular(8))),
+                          border: Border.all(width: 1, color: Colors.pink),
+                          borderRadius:
+                              const BorderRadius.all(const Radius.circular(8)),
+                        ),
                         width: 170,
                         height: 120,
                       ),
@@ -206,28 +222,32 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(left: 20),
-                              child: Text("Comments"),
-                            ),
-                            Container(
-                              child: Icon(
-                                Icons.edit_rounded,
-                                size: 35,
+                      GestureDetector(onTap: () {
+                        Navigator.pushNamed(context, '/CommentsManagements');
+                      },
+                        child: Container(
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 20),
+                                child: Text("Comments"),
                               ),
-                              margin: EdgeInsets.only(left: 20),
-                            ),
-                          ],
+                              Container(
+                                child: Icon(
+                                  Icons.edit_rounded,
+                                  size: 35,
+                                ),
+                                margin: EdgeInsets.only(left: 20),
+                              ),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 1, color: Colors.pink),
+                              borderRadius: const BorderRadius.all(
+                                  const Radius.circular(8))),
+                          width: 170,
+                          height: 120,
                         ),
-                        decoration: BoxDecoration(
-                            border: Border.all(width: 1, color: Colors.pink),
-                            borderRadius: const BorderRadius.all(
-                                const Radius.circular(8))),
-                        width: 170,
-                        height: 120,
                       ),
                     ],
                   ),
