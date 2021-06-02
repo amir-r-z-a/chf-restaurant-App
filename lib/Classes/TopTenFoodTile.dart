76cf09f926e25e88ce815a15e01d569491114e94
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2021-06-02 21:47:23 +0430

    default image added

--- lib/Classes/TopTenFoodTile.dart
+++ lib/Classes/TopTenFoodTile.dart
@@ -11,17 +11,16 @@ class TopTenFoodTile extends StatefulWidget {
   String _price;
   String desc;
   bool _foodStatus;
-  Image _foodImage;
+  Image foodImage ;
   int orderCount;
   int _rank;
   static Function function;
-
   TopTenFoodTile(
       this._name, this._price, this._foodStatus, this._category, this._rank,
-      {this.desc, this.orderCount = 0});
-
+      {this.desc, this.orderCount = 0,this.foodImage  }){
+    this.foodImage=Image.asset("assets/images/9.jpg");
+  }
   String get name => _name;
-
   set name(String value) {
     _name = value;
   }
@@ -44,11 +43,6 @@ class TopTenFoodTile extends StatefulWidget {
     _category = value;
   }
 
-  Image get foodImage => _foodImage;
-
-  set foodImage(Image value) {
-    _foodImage = value;
-  }
 
   int get rank => _rank;
 
@@ -84,62 +78,69 @@ class _TopTenFoodTileState extends State<TopTenFoodTile> {
         DetailsTopTenFoodTile.orderCount = widget.orderCount;
         Navigator.pushNamed(context, '/DetailsTopTenFoodTile');
       },
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.symmetric(
-              horizontal: BorderSide(color: Color(0xffDCDCDC), width: 1)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Padding(padding: EdgeInsets.symmetric(vertical: 3)),
-            Row(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 10,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.symmetric(
+                  horizontal: BorderSide(color: Color(0xffDCDCDC), width: 1)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Padding(padding: EdgeInsets.all(7)),
-                Text(
-                  widget.category,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                Padding(padding: EdgeInsets.symmetric(vertical: 3)),
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(7)),
+                    Text(
+                      widget.category,
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28,fontFamily:'HotPizza'),
+                    ),
+                  ],
+                ),
+                Row(children: [
+                  Padding(padding: EdgeInsets.all(7)),
+                  Container(
+                    child: widget.foodImage ,
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    height: 95,
+                    width: 95,
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(Accounts.digester(widget.name,
+                          MediaQuery.of(context).size.width.floor() - 372)),
+                      Padding(padding: EdgeInsets.all(3)),
+                      Text(Accounts.digester(
+                          widget.desc == null ? ' ' : widget.desc,
+                          MediaQuery.of(context).size.width.floor() - 372)),
+                      Padding(padding: EdgeInsets.all(13)),
+                      Text(Accounts.digester(
+                          'Price: ' + '\$' + widget.price.toString(),
+                          MediaQuery.of(context).size.width.floor() - 372)),
+                    ],
+                  ),
+                ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(widget.rank.toString()),
+                    Padding(padding: EdgeInsets.all(7)),
+                  ],
                 ),
               ],
             ),
-            Row(children: [
-              Padding(padding: EdgeInsets.all(7)),
-              Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.black)),
-                height: 95,
-                width: 95,
-              ),
-              Padding(padding: EdgeInsets.all(5)),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(Accounts.digester(widget.name,
-                      MediaQuery.of(context).size.width.floor() - 372)),
-                  Padding(padding: EdgeInsets.all(3)),
-                  Text(Accounts.digester(
-                      widget.desc == null ? ' ' : widget.desc,
-                      MediaQuery.of(context).size.width.floor() - 372)),
-                  Padding(padding: EdgeInsets.all(13)),
-                  Text(Accounts.digester(
-                      'Price: ' + '\$' + widget.price.toString(),
-                      MediaQuery.of(context).size.width.floor() - 372)),
-                ],
-              ),
-            ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(widget.rank.toString()),
-                Padding(padding: EdgeInsets.all(7)),
-              ],
-            ),
-          ],
+            // color: Colors.green,
+            height: 190,
+            width: MediaQuery.of(context).size.width,
+          ),
         ),
-        // color: Colors.green,
-        height: 190,
-        width: MediaQuery.of(context).size.width,
       ),
     );
   }
